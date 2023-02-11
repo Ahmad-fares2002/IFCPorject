00b914e28e7bf2ee006e33f16b4f9f28da757276
Ahmad Fares <ahmadfares355@gmail.com>
2023-02-12 02:13:22 +0300

    First push

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { Color } from "three";
+import { IfcViewerAPI } from "web-ifc-viewer";
+
+const container = document.getElementById("viewer-container");
+const viewer = new IfcViewerAPI({
+  container,
+  backgroundColor: new Color(0xffffff),
+});
+viewer.axes.setAxes();
+viewer.grid.setGrid();
+
+const input = document.getElementById("file-input");
+
+window.ondblclick = () => viewer.IFC.selector.pickIfcItem(true);
+window.onmousemove = () => viewer.IFC.selector.prePickIfcItem();
+viewer.clipper.active = true;
+viewer.IFC.setWasmPath('../wasm')
+
+
+// window.onkeydown = (event) => {
+//   if (event.code === "KeyP") {
+//     viewer.clipper.createPlane();
+//   } else if (event.code === "KeyO") {
+//     viewer.clipper.deletePlane();
+//   }
+// };
+
+input.addEventListener('change',  changed => {
+  const file = changed.target.files[0]
+  viewer.IFC.loadIfc(file)
+})
